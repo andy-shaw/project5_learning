4c7dc81cbe88f320612fca57b92c9022b4b89315
Andy Shaw <c00lnerd314@gmail.com>
2013-11-20 18:01:15 -0500

    add state class

--- _transitionTest.py
+++ _transitionTest.py
@@ -1,12 +1,19 @@
 from transition import *
 from place import Place
 import os
+import sys
 
+debug = False
+try:
+    y = sys.argv[1]
+    if y: debug = True
+except:
+    pass
 
 file = open(os.getcwd() + r'\Input_Files\deterministic_transitions.txt', 'r')
 input = file.readlines()
 
-transitions = []
+dtransitions = []
 
 for line in input:
     x = line.rstrip().split(',')
@@ -17,20 +24,23 @@ for line in input:
     for i in range(2, len(x), 2):
         pairs.append((int(x[i]), float(x[i+1])))
     
-    transitions.append(Transition(fromPlace, action, pairs))
+    dtransitions.append(Transition(fromPlace, action, pairs))
     
-for trans in transitions:
+for trans in dtransitions:
     p = 0.0
     for place in trans.getDestinationPlaces():
         p = p + trans.probabilityOfPlace(place)
     assert (p == 1.0)
+    if debug: print trans.toString()
     
 file.close()
+
+if debug:print '========================================'
 
 file = open(os.getcwd() + r'\Input_Files\non-deterministic_transistions.txt', 'r')
 input = file.readlines()
 
-transitions = []
+ndtransitions = []
 for line in input:
     x = line.rstrip().split(',')
     
@@ -40,19 +50,26 @@ for line in input:
     for i in range(2, len(x), 2):
         pairs.append((int(x[i]), float(x[i+1])))
     
-    transitions.append(Transition(fromPlace, action, pairs))
+    ndtransitions.append(Transition(fromPlace, action, pairs))
+    if debug: print trans.toString()
 
 #test for probabilities summing to 1 and correct input
-for trans in transitions:
+for trans in ndtransitions:
     p = 0.0
     for place in trans.getDestinationPlaces():
         p += trans.probabilityOfPlace(place)
 
     assert(str(p) == '1.0') #BIG HACK  ---- do not try at home, kids
     
+if debug: print '========================================'
+
+#test getTransitions
 file = open(os.getcwd() + r'\Input_Files\places.txt', 'r')
 input = file.readlines()
 places = []
+
+
+
 
 for line in input:
     x = line.rstrip().split(',')
@@ -67,8 +84,12 @@ for line in input:
     
 for place in places:
     id = place.getId()
-    x = getTransitions(id, transitions)
+    x = getTransitions(id, ndtransitions)
+    if debug: print id, '\n----'
     for trans in x:
+        if debug: print trans.toString()
         assert(id == trans.getFromPlace())
+    if debug: print '----------------------------------------------------------------'
+    
     
 file.close()
--- state.py
+++ state.py
@@ -3,19 +3,42 @@ Author: Andy Shaw
 Date:   11/18/2013
 Course: CSE 3521 - Assignment 04
 
-The state holds the current weight/value of the location, the place, and the possible outgoing
+The state holds the current utility/value of the location, the place, and the possible outgoing
 transitions.
 '''
 
 class State:
-    '''Weight, place, and possible transitions'''
-    def __init__(self, weight, place, transitions):
-        self.weight = weight
+    '''utility, place, and possible transitions'''
+    def __init__(self, utility, place, transitions):
+        self.utility = utility
         self.place = place
+        self.id = place.getId()
         self.transitions = transitions
+        
+    def getUtility(self):
+        return self.utility
+        
+    def getPlace(self):
+        return self.place
+        
+    def getId(self):
+        return self.id
         
     def getTransitions(self):
         return transitions
         
-    def updateWeight(self):
-        self.weight = 
+    '''
+    This method will update the utility of the State.  This algorithm will look at destination
+    states, so all states in the environment need to be passed.
+    
+    param: allStates all states that are in the environment
+    '''
+    def updateUtility(self, allStates):
+        '''utility = reward + maxAction(sum of all probabilities * neighbor utility)'''
+        reward = self.place.getReward()
+        
+        possibleActions = []
+        #get all actions
+        for transition in self.transitions:
+            for destination in transition.getDestinationPlaces():
+                
